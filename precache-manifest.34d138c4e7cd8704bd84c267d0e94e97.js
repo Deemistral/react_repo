be23a1afa69af7e7deb784e94051629375b48a7f
Deemistral <olaniyiadedolapo@gmail.com>
2020-09-17 08:56:44 +0100

    Updates

---
+++ precache-manifest.34d138c4e7cd8704bd84c267d0e94e97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f206304ed540dfa03abdee753a67376",
+    "url": "/react_repo/index.html"
+  },
+  {
+    "revision": "ce1f7d6f36d0276f41c6",
+    "url": "/react_repo/static/css/main.65c3c8d4.chunk.css"
+  },
+  {
+    "revision": "e4a6acb6a9f7df76e39c",
+    "url": "/react_repo/static/js/2.1d64572c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_repo/static/js/2.1d64572c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce1f7d6f36d0276f41c6",
+    "url": "/react_repo/static/js/main.1d97b984.chunk.js"
+  },
+  {
+    "revision": "6db9e4d3351d3c3bc79d",
+    "url": "/react_repo/static/js/runtime-main.c652ba76.js"
+  }
+]);
